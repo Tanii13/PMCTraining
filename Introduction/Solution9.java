817e282a51396dc2dd4ecb5680bb51274006d2fd
Tanii13 <117333951+Tanii13@users.noreply.github.com>
2022-11-04 12:09:42 +0530

    Add files via upload

---
+++ Introduction/Solution9.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution9 {
+
+    public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    int count =1;
+    while(sc.hasNext()){
+        String s = sc.nextLine();
+        System.out.println(count+" "+s);
+        count++; /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
+}
